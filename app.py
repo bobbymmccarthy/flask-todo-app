e519ce2a27a1e778b0f3629874ada2b219925b15
bobbymmccarthy <bmccarthy@college.harvard.edu>
2023-02-15 09:06:32 -0500

    added comments

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#application factories
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -13,46 +14,56 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
 
         
-
+#routing 
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list = todo_list)
 
+#routing
 @app.route('/about')
 def about():
     return 'About'
 
+#routing
 @app.route('/add', methods = ["POST"])
 def add():
     title = request.form.get('title')
+    #application context and application factories
     with app.app_context():
         new_todo = Todo(title = title)
         db.session.add(new_todo)
         db.session.commit()
-
+    #redirection
     return redirect(url_for('index'))
 
+#routing
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
     title = request.form.get('title')
+    #application context and application factories
     with app.app_context():
+        #database query
         todo = Todo.query.filter_by(id=todo_id).first()
         todo.complete = not todo.complete
-        print(todo.complete)
         db.session.commit()
 
+    #redirection
     return redirect(url_for('index'))
 
+#routing
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
     title = request.form.get('title')
+    #application context and application factories
     with app.app_context():
+        #database query
         todo = Todo.query.filter_by(id=todo_id).first()
         db.session.delete(todo)
         db.session.commit()
 
+    #redirection
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
